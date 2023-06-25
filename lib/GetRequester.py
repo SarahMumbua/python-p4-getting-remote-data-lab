71d95007a816be192cbb8c984b352563140a1969
=SarahMumbua <mumbuasarah2030@gmail.com>
2023-06-25 17:07:51 +0300

    completed lab

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,14 @@
-import requests
-import json
+import requests,json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        json_data = json.loads(response_body)
+        return list(json_data)
